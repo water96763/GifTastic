027b7963b8b28f8a6ad2104723a3d891ea973cf9
Valerie Sakamura <water96763@gmail.com>
2019-07-20 22:31:32 -0700

    ajax works, and buttons work but showing data is not working

--- javascript.js
+++ javascript.js
@@ -1,27 +1,53 @@
-// let topics = [];
-// $("#createBtn").on("click", function(event) {
-//     event.preventDefault();
-//     var fruitType = $("#createBtn").val().trim();
-//     topics.push(fruitType);
+let topics = [];
 
-// console.log(topics);
-// });
-
-// var createButton = function() {
-//   var newButton = $("<btn>");
-   
+// function alertFruitName() {
+//   let fruitName = $(this).attr("data-name");
 // }
-var searchGiphy = function(fruit) {
+
+
+function renderButtons() {
+  $("#gifInfo").empty();
+
+  for (var i = 0; i < topics.length; i++) {
+    var a = $("<button>");
+    a.addClass("fruit");
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
+    $("#gifInfo").append(a);
+  }
+}
+
+function searchGiphy() {
+  
+  var fruit = $(this).attr("data-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hDcyYQDAb4wguGxBI8nGwH1wyhq0SA1g&q=" + fruit + "&limit=10&lang=en";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    console.log(response.rating);
+    var fruitDiv = $("<div class='fruit'>")
+    var rating = response.rating;
+    var pOne = $("<p>").text("Rating: " + rating);
+    fruitDiv.append(pOne);
+    var imgURL = response.original_still.url;
+    var image = $("<img>").attr("src", imgURL);
+    fruitDiv.append(image);
 
+    
+    console.log(response.rating);
+    $("#gifInfo").text(JSON.stringify(response));
   });
 };
-  searchGiphy("banana");
-  searchGiphy("papaya");
 
+  $("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var fruit = $("#gif-search").val().trim();
+    topics.push(fruit);
+    renderButtons();
+    // searchGiphy();
+});
+
+$(document).on("click", ".fruit", searchGiphy);
+
+renderButtons();
