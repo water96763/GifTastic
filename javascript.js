23aba9d89e091e3a8ff1bc0ef9ff54d0504fdca9
Valerie Sakamura <water96763@gmail.com>
2019-07-20 20:01:22 -0700

    ajax works

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
-let topics = [];
-$("#fruit").on("click", function(event) {
-    event.preventDefault();
-    var fruitType = $("#fruit").val().trim();
-    topics.push(fruitType);
+// let topics = [];
+// $("#createBtn").on("click", function(event) {
+//     event.preventDefault();
+//     var fruitType = $("#createBtn").val().trim();
+//     topics.push(fruitType);
 
-console.log(topics);
-});
+// console.log(topics);
+// });
 
-$.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?api_key=hDcyYQDAb4wguGxBI8nGwH1wyhq0SA1g&q=" + topics + "&limit=10" + "&lang=en",
+// var createButton = function() {
+//   var newButton = $("<btn>");
+   
+// }
+var searchGiphy = function(fruit) {
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hDcyYQDAb4wguGxBI8nGwH1wyhq0SA1g&q=" + fruit + "&limit=10&lang=en";
+  $.ajax({
+    url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    console.log(response.rating);
+
   });
+};
+  searchGiphy("banana");
+  searchGiphy("papaya");
 
